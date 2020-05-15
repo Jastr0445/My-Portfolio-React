eccf52fbc8a01245164dcee5ffc82ba36cde4d40
Jackson Strauss <58577918+Jastr0445@users.noreply.github.com>
2020-05-15 16:30:11 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from './components/nav';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+
+    
+      
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/homepage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+render() {
+    return(
+
+
+
+    )
+
+ }
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
