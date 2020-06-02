6b5ade19988929788031c16662b897bb66d7f159
Jackson Strauss <58577918+Jastr0445@users.noreply.github.com>
2020-06-02 12:16:42 -0500

    Add files via upload

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => (
+  <div>
+    <h1>Contact me</h1>
+    <ul>  
+     <li><strong><a href="mailto:jastr0445@gmail.com">email</a></strong></li>
+     <li> <strong><a href="https://github.com/Vaultboy58">github</a></strong></li>
+     <li><strong><a href="https://www.linkedin.com/in/jackson-strauss-560aa81a4/detail/background-image/">linkedin</a></strong></li>
+     </ul>  
+  </div>
+);
+
+export default Contact;
